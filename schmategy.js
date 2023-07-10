0d92c33fb4b34d919455fd728c99be6436001e57
Barry Prendergast <barry.m.prendergast@gmail.com>
2023-07-10 18:00:14 +0200

    first push

---
+++ schmategy.js
@@ -0,0 +1,17 @@
+const randomItem = (array) => array[Math.floor(Math.random() * array.length)];
+
+let data;
+
+fetch("topsecret.json")
+  .then((response) => response.json())
+  .then((json) => (data = json));
+
+const generateStrategy = () => {
+  return `To ${randomItem(data.objectives)}, we will ${randomItem(
+    data.actions
+  )} ${randomItem(data.when)}. This will ${randomItem(data.impacts)}.`;
+};
+
+document.getElementById("generate-btn").addEventListener("click", function () {
+  document.getElementById("display-strategy").innerText = generateStrategy();
+});
